946c9d7e2e61d699e47c67c0dad1f4b9c42a6d18
debasish ghosh <profile.dghosh@gmail.com>
2021-09-23 20:14:48 -0400

    added main.yml

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA0U1ZvR60zeb1_4v2h1cZAEFY1YfzzKE0",
+    authDomain: "dpgallery-3893e.firebaseapp.com",
+    projectId: "dpgallery-3893e",
+    storageBucket: "dpgallery-3893e.appspot.com",
+    messagingSenderId: "70882688717",
+    appId: "1:70882688717:web:292453fae7e4668c56f384",
+    measurementId: "G-9TRVME6VLQ"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA0U1ZvR60zeb1_4v2h1cZAEFY1YfzzKE0",
+    authDomain: "dpgallery-3893e.firebaseapp.com",
+    projectId: "dpgallery-3893e",
+    storageBucket: "dpgallery-3893e.appspot.com",
+    messagingSenderId: "70882688717",
+    appId: "1:70882688717:web:292453fae7e4668c56f384",
+    measurementId: "G-9TRVME6VLQ"
+  }
 };
 
 /*
